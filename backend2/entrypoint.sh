f3f198a3bec2886f85cae32ea72dd952552aaf2b
luisenrique-varelarodriguez <trabajo.luisen@gmail.com>
2025-02-27 20:26:34 +0100

    chore: Update backend2 service to use Gunicorn and pin dependency versions

--- backend2/entrypoint.sh
+++ backend2/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8001
+#python manage.py runserver 0.0.0.0:8001
+gunicorn api.wsgi:application --bind 0.0.0.0:8001 --workers 3 --log-level debug
