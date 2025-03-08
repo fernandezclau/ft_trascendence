2d4122d27800ccfed1c63cd4e91cee2450e7b5ad
luisenrique-varelarodriguez <trabajo.luisen@gmail.com>
2025-03-08 13:23:56 +0100

    add prometheus, grafana and node-exporter services and config [WORKING]

--- auth-local/api/urls.py
+++ auth-local/api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import login_user, register_user, get_csrf_token
 
 urlpatterns = [
     path("api/auth/register", register_user, name="register_user"),
     path("api/auth/login", view=login_user, name="login_user"),
-    path("api/auth/csrf/", get_csrf_token, name="get_csrf_token"), 
+    path("api/auth/csrf/", get_csrf_token, name="get_csrf_token"),
+    path("", include("django_prometheus.urls")),
 ]
