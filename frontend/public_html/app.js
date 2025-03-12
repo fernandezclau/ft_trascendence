2798fd7632856378a1388c3a12bbf8fbc9d84345
fernandezclaudia <cfernandezg@bcds.net>
2025-03-12 15:44:06 +0100

    register get request

--- frontend/public_html/app.js
+++ frontend/public_html/app.js
@@ -36,6 +36,15 @@ const PageManager = (() => {
                 loadGame();
             else if (page == "tournament")
                 loadTournament();
+            else if (page == "register")
+            {
+                let register = document.getElementById("registerForm");
+                if (register) {
+                    register.addEventListener("submit", function(event) {
+                        event.preventDefault();
+                    });
+                } 
+            }
 
             toggleGameVisibility(page === "game" || page === "tournament");
     
